619fe332dbcb345d25160c0d1a8f557e352c0f0d
Sam Ellard <sam.ellard@gmail.com>
2024-02-11 08:20:57 +0900

    First commit on Ecomm demo

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # Added by devise Admin
+  devise_for :admins
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+  get "admin" => "admin#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
